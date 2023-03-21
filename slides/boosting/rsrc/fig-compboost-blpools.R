c4f3810be423f1942f96665dff7f7f08bc03ae8b
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2023-03-21 16:06:59 +0100

    adjustments to bias-variance-decomp and loss-properties chunks

--- slides/boosting/rsrc/fig-compboost-blpools.R
+++ slides/boosting/rsrc/fig-compboost-blpools.R
@@ -27,3 +27,31 @@ invisible(lapply(seq_along(fs), function(i) {
   ggsave(sprintf("boosting-cwb-blpool%s.png", i), gg, width = 1.9, height = 1.3)
 }))
 
+set.seed(31415)
+gg1 = ggplot() +
+  geom_point(aes(x = x, y = fs[[1]](x) + rnorm(length(x), 0, 1)), alpha = 0.2) +
+  geom_line(aes(x = x, y = fs[[1]](x)), linewidth = 1.1) +
+  xlab(expression(x[1])) +
+  ylab(expression(paste(b[1], "(", x[1], "|", theta["  1"]^"  [m]", ")"))) +
+  theme_minimal()
+
+ggsave("boosting-cwb-bl1-points.png", gg1, width = 1.9, height = 1.3)
+
+gg1 = ggplot() +
+  geom_point(aes(x = x, y = fs[[2]](x) + rnorm(length(x), 0, 1)), alpha = 0.2) +
+  geom_line(aes(x = x, y = fs[[2]](x)), linewidth = 1.1) +
+  xlab(expression(x[2])) +
+  ylab(expression(paste(b[2], "(", x[2], "|", theta["  2"]^"  [m]", ")"))) +
+  theme_minimal()
+
+ggsave("boosting-cwb-bl2-points.png", gg1, width = 1.9, height = 1.3)
+
+gg1 = ggplot() +
+  geom_point(aes(x = x, y = fs[[3]](x) + rnorm(length(x), 0, 1)), alpha = 0.2) +
+  geom_line(aes(x = x, y = fs[[3]](x)), linewidth = 1.1) +
+  xlab(expression(x[3])) +
+  ylab(expression(paste(b[3], "(", x[3], "|", theta["  3"]^"  [m]", ")"))) +
+  theme_minimal()
+
+ggsave("boosting-cwb-bl3-points.png", gg1, width = 1.9, height = 1.3)
+
