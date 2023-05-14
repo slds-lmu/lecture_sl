65e925fa1b83c54ca0a7a4a9546a1bfca9a92c23
patricio.massaro <patomassaro@gmail.com>
2023-05-14 13:44:58 +0200

    Adding figures and scripts for feature selection

---
+++ slides/feature-selection/rscr/fs-filters-guyon-examples.R
@@ -0,0 +1,62 @@
+# check if thelibraries are installed, otherwise install
+if (!requireNamespace("GGally")) {
+  install.packages("GGally")
+}
+if (!requireNamespace("ggplot2")) {
+  install.packages("ggplot2")
+}
+if (!requireNamespace("mvtnorm")) {
+  install.packages("mvtnorm")
+}
+
+library(GGally)
+library(ggplot2)
+library(mvtnorm)
+variance=1
+n_data=1500
+
+covariance_matrix = matrix(c(variance,
+                             0.96*variance,0.96*variance,
+                             variance),
+                           ncol= 2)
+
+# Create a vector of 50 1 and 50 0s
+y = c(rep(1,n_data/2),rep(0,n_data/2))
+# Create a multi-variate normal distribution with mean 0 and covariance matrix 1
+x = rmvnorm(n_data,
+            mean = c(0,0),
+            sigma = covariance_matrix
+            )
+x1 = x[,1]
+x2 = x[,2]
+# add a constant if y is 1 to x2
+x2[y==1] = x2[y==1]+2*variance
+# Create a dataframe with x and y
+data = data.frame(x1=x1,x2=x2,y=y)
+data$y = as.factor(data$y)
+
+
+custom_hist <- function(data, mapping, ...){
+  ggplot(data = data, mapping = mapping) +
+    geom_histogram(data = data[data$y == 0,], fill = "#F8766D", alpha = 0.6) +
+    geom_histogram(data = data[data$y == 1,], fill = "#00BFC4", alpha = 0.6)
+}
+
+
+# Use ggpairs only for x and x1 and show the inverted scatterplot
+graph<- ggpairs(data = data,
+                columns = c("x1","x2"),
+                legend=2,
+                aes(group = y,colour=y, alpha = 0.6),
+                upper = list(continuous = wrap("points", alpha = 0.6)),
+                diag = list(continuous = custom_hist)
+                )+
+  theme(legend.position = "bottom")
+
+#Save the plot
+ggsave(filename = "slides/feature-selection/figure/guyon_example_correlation.png",
+       plot = graph,
+       width = 10, height = 10, units = "cm")
+
+
+
