950489828cd12550967db573716a28d140973953
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2023-09-06 18:03:50 +0200

    add figure for S function (L1 loss)

--- slides/advriskmin/rsrc/optimal-constant-model-variance.R
+++ slides/advriskmin/rsrc/optimal-constant-model-variance.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 
 set.seed(123)
-n = 4
+n = 5
 x <- -n:n
-y <- x+rnorm(n, sd = 1)
+y <- x+rnorm(length(x), sd = 1)
 beta_0 <- mean(y)
 
 bias_only_model <- function(x) {
@@ -25,8 +25,8 @@ df$error_abs <- abs(df$error)
 
 
 p <- ggplot(df, aes(x=x, y=y)) + 
-  geom_point(aes(color=DATA_COLOR), size = 3) + 
-  geom_line(aes(y=prediction), color=MODEL_COLOR, size = 1.2) + 
+  geom_point(aes(color=DATA_COLOR), size = 4) + 
+  geom_line(aes(y=prediction), color=MODEL_COLOR, size = 1.4) + 
   geom_rect(aes(
     xmin = bottom_left_x,
     xmax = bottom_left_x + error_abs,
@@ -35,17 +35,20 @@ p <- ggplot(df, aes(x=x, y=y)) +
     fill = LINEAR_MODEL_ERROR_COLOR
   ), alpha = 0.4) +
   labs(title="Optimal constant model gives ''unscaled'' variance") +
+  xlab("") +
   scale_color_identity() +
   scale_fill_identity() +
   theme_minimal() +
   xlim(-5,5) + ylim(-5,5) +
   theme(
-    axis.text = element_text(size = rel(1.7)), 
-    title = element_text(size = rel(2))     
+    axis.text = element_text(size = rel(3)), 
+    title = element_text(size = rel(2.1))     
     #,panel.grid.major = element_blank()       
-    #,panel.grid.minor = element_blank()         
+    ,panel.grid.minor = element_blank()         
   ) + 
-  scale_x_continuous(breaks = seq(-5, 5, by = 1)) + # x-axis increments of 1
-  scale_y_continuous(breaks = seq(-5, 5, by = 1))  # y-axis increments of 1
+  scale_x_continuous(breaks = seq(-6, 6, by = 1)) + # x-axis increments of 1
+  scale_y_continuous(breaks = seq(-6, 6, by = 1))  # y-axis increments of 1
 
 ggplot2::ggsave("../figure_man/plot_const_var.png", p, height = 10L, width = 10L)
+
+p
