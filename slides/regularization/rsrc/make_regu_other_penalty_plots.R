330dd082dd7280d290f418ca31ec759e0a3bb49b
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2024-05-10 19:27:00 +0200

    Updates from Overleaf

--- slides/regularization/rsrc/make_regu_other_penalty_plots.R
+++ slides/regularization/rsrc/make_regu_other_penalty_plots.R
@@ -8,31 +8,40 @@ library(MASS)
 ndata <- 200
 ncoef <- 1000
 beta <- c(4, 2, -4, -2, rep(0, ncoef-4))
-set.seed(123)
-mean_vec <- sample(0:10, size=ncoef, replace=TRUE)
-set.seed(110)
-X <- mvrnorm(n=ndata, mu=mean_vec, Sigma=diag(ncoef))
+numsignals = sum(beta != 0)
+lam_min = 0.1
+set.seed(42)
+X =  matrix(rnorm(ndata*ncoef), ncol=ncoef)
 
-epsilon <- rnorm(200)
+epsilon <- rnorm(ndata)
 y <- X %*% beta + epsilon
 
+linewidth = 4
+fontsize = 2
 
-### lasso
-fit_lasso <- ncvreg(cbind(1,X), y, penalty="lasso", lambda.min=0.1)
-plot(fit_lasso, main="Lasso", ylab="coef value")
+### Lasso
+fit_lasso <- ncvreg(X, y, penalty="lasso", lambda.min=lam_min)
+plot(fit_lasso, main=expression("Lasso"), ylab=expression(theta), 
+     cex.lab=fontsize, cex.main=1.2*fontsize, cex.axis=fontsize, 
+     lwd=linewidth, log.l=TRUE) 
+abline(h = c(-4, -2, 2, 4), col = "grey", lty = 8)
 dev.copy(png, "../figure/other-pen-lasso.png")
 dev.off()
 
-
 ### MCP
-fit_mcp <- ncvreg(X, y, penalty="MCP", gamma=3, lambda.min=0.07)
-plot(fit_mcp, main=expression(MCP~gamma~"=3"), ylab="coef value")
+fit_mcp <- ncvreg(X, y, penalty="MCP", gamma=3.1, lambda.min=lam_min)
+plot(fit_mcp, main=expression("MCP"~~(gamma~"=3.1")), ylab=expression(theta), 
+     cex.lab=fontsize, cex.main=1.2*fontsize, cex.axis=fontsize, 
+     lwd=linewidth, log.l=TRUE) 
+abline(h = c(-4, -2, 2, 4), col = "grey", lty = 8)
 dev.copy(png, "../figure/other-pen-MCP.png")
 dev.off()
 
-
 ### SCAD
-fit_scad <- ncvreg(X, y, penalty="SCAD", gamma=4, lambda.min=0.07)
-plot(fit_scad, main=expression(SCAD~gamma~"=4"), ylab="coef value")
+fit_scad <- ncvreg(X, y, penalty="SCAD", gamma=3.8, lambda.min=lam_min)
+plot(fit_scad, main=expression("SCAD"~~(gamma~"=3.8")), ylab=expression(theta), 
+     cex.lab=fontsize, cex.main=1.2*fontsize, cex.axis=fontsize, 
+     lwd=linewidth, log.l=TRUE) 
+abline(h = c(-4, -2, 2, 4), col = "grey", lty = 8)
 dev.copy(png, "../figure/other-pen-SCAD.png")
 dev.off()
