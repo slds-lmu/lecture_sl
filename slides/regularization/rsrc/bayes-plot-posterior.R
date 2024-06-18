919496e86d85be1fa3ed97b9afa8131874b961be
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2024-06-18 12:50:49 +0200

    update bayes plot + code and comment on L2 chunk plot

--- slides/regularization/rsrc/bayes-plot-posterior.R
+++ slides/regularization/rsrc/bayes-plot-posterior.R
@@ -1,17 +1,24 @@
+### Simulate n samples from y = theta + eps where theta=1 and eps is N(0,1)
+### Use 3 different values tau2 for Gaussian prior variances of theta
+### Compute posterior of theta using empirical mean update formula
+### compare MAP and ridge loss surface/minimizer
+
 library(ggplot2)
+library(ggpubr)
+
+set.seed(10)
 
 # Define prior variances
 tau2 <- c(0.25^2, 0.5^2, 2^2)
 taus <- sqrt(tau2)
 
 # Simulate data
-set.seed(42)
 n <- 20
 sigma2 <- 1
 y <- rnorm(n, mean = 1, sd = sqrt(sigma2))
 y_bar <- mean(y)
 
-# Generate x-axis sequence
+# Generate x-axis sequence for range of theta values
 x_seq <- seq(-3, 3, length.out = 300)
 
 # df for plotting priors
@@ -49,10 +56,10 @@ loss_df <- data.frame(
 # Max loss to scale second y-axis
 max_loss <- max(loss_df$loss)
 
-# Prior densities
+# Plot for prior densities
 p1 <- ggplot(prior_df, aes(x = x, y = density, color = as.factor(tau))) +
   geom_line(linewidth=1.5) +
-  labs(title = "Prior Densities", y = "Density", x = "Theta", color = "Prior Std. Dev.") +
+  labs(title = "Prior Densities", y = "Density", x = expression(theta), color = "Prior Std. Dev.") +
   theme_minimal() +
   theme(
     plot.title = element_text(size = 20, hjust=0.5),
@@ -62,12 +69,12 @@ p1 <- ggplot(prior_df, aes(x = x, y = density, color = as.factor(tau))) +
     legend.text = element_text(size = 16)
   )
 
-# Posterior denisities + ridge risk
+# Plot for posterior densities + ridge risk
 p2 <- ggplot(posterior_df, aes(x = x, y = density, color = as.factor(tau))) +
   geom_line(linewidth = 1.5) +
   geom_vline(data = data.frame(tau = as.factor(taus), xintercept = posterior_mean), aes(xintercept = xintercept, color = tau), linetype = "dashed", size = 1.2) +
   geom_line(data = loss_df, aes(x = theta, y = loss / max_loss * max(posterior_df$density), color = as.factor(tau)), linetype = "solid", size=1) +
-  labs(title = "Posterior Densities (MAP=Ridge)", y = "Density", x = "Theta", color = "Std. Dev.") +
+  labs(title = "Posterior Densities (MAP=Ridge)", y = "Density", x = expression(theta), color = "Std. Dev.") +
   scale_y_continuous(sec.axis = sec_axis(~ . / max(posterior_df$density) * max_loss, name = "Reg. Emp. Risk", breaks = scales::pretty_breaks())) +
   theme_minimal() +
   xlim(-1,3) +
@@ -79,8 +86,7 @@ p2 <- ggplot(posterior_df, aes(x = x, y = density, color = as.factor(tau))) +
     legend.text = element_text(size = 16)
   )
 
-# Combine plots
-library(ggpubr)
+# Combine plots and save
 p.comb <- ggarrange(p1,p2, ncol=2, hjust=-0.5, common.legend=TRUE)
+ggsave(filename = paste0("../figure/bayes-plot-posterior.png"), plot = p.comb, width = 12, height = 4) 
 
-print(p.comb)
