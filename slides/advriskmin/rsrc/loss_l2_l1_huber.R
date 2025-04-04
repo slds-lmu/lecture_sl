65effa84c609cf5bea15966d9aedcecef6d9d677
Bernd Bischl <bernd_bischl@gmx.net>
2025-04-04 11:33:12 +0200

    advrisk:loss-props better plot for robustness

---
+++ slides/advriskmin/rsrc/loss_l2_l1_huber.R
@@ -0,0 +1,17 @@
+library(ggplot2)
+
+source("helpers/loss_functions.R")
+
+df = data.frame(res = seq(-10L, 10L, length.out = 800L))
+
+losses <- list(
+  "L1" = function(res) L1(res),
+  "L2" = function(res) L2(res),
+  "Huber" = function(res) Huber(res, c = 5)
+)
+
+
+pl = plotLoss(df, losses) 
+
+ggplot2::ggsave("../figure/loss_l2_l1_huber.png", pl)
+
